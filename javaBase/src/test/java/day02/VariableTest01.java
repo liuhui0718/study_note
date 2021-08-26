fc725ca2f156ed215ead21e8a858fc6fb0776f7e
liuhui718 <1751180430@qq.com>
2021-08-26 23:32:14 +0800

    关键字、保留字、标识符的命名规则与规范、变量的使用与划分

---
+++ javaBase/src/test/java/day02/VariableTest01.java
@@ -0,0 +1,23 @@
+package day02;
+
+/*
+java定义的数据类型
+一：变量按照数据类型来分：
+    基本数据类型
+        整型：byte,short,int,long
+        浮点型：float，double
+        字符型：char
+        布尔型：boolean
+
+    引用数据类型
+        类(class)：
+        接口(interface)：
+        数组(array)：
+
+二：按照在类中声明的位置来分：
+    成员变量  vs   局部变量
+ */
+
+public class VariableTest01 {
+
+}
